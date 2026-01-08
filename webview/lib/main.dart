cd830ca8c03b0f0be59cdc3aea2c10d3800670b5
ying <ying_zhang@cmu.ac.th>
2026-01-08 15:27:38 +0800

    Basic Flutter WebView Example

---
+++ webview/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyWebView(),
+    );
+  }
+}
+
+class MyWebView extends StatefulWidget {
+  const MyWebView({super.key});
+
+  @override
+  State<MyWebView> createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView Example'),
+      ),
+      body: WebViewWidget(
+        controller: _controller,
+      ),
+    );
+  }
+}
